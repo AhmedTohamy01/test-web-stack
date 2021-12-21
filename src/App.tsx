2273f7e8965cf6cba0126a3d70c1d30529b6b40f
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2021-12-21 19:34:08 +0200

    feat [App] : Import MainPage | add default route redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import MainPage from './Pages/MainPage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
 const App = () => {
-  return <>Hello App</>
+  return (
+    <Router>
+      <Switch>
+        <Route path='/limit=*'>
+          <MainPage />
+        </Route>
+        <Route path='/'>
+          <Redirect to={{ pathname: '/limit=6' }} />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
